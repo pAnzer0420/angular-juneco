bdc5c69a8b4b10ed9aea2b5332ec4717682f114f
angular-juneco <sheepr6@gmail.com>
2024-04-20 11:16:43 +0800

    final

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import axios from 'axios';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
 })
@@ -13,12 +14,16 @@ export class TodoComponent {
   todoItem = new FormControl('', Validators.required);
   todoList: string[] = [];
 
-  pushItemToList() {
-    const todoItemValue = this.todoItem.value;
-
-    if (todoItemValue && todoItemValue.trim() !== '') {
-      this.todoList.push(todoItemValue);
+  async pushItemToList() {
+    const todoItem = this.todoItem.value;
+    try {
+      const response = await axios.post('http://localhost:3000/todo', {
+    itemName: todoItem,
+      });
+      this.todoList.push(response.data);
       this.todoItem.reset();
+    } catch (error) {
+      console.log('Error', error);
     }
   }
 }

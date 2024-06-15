357810ea9f09c64ea6b8c4221f25ae260ec077f7
angular-juneco <sheepr6@gmail.com>
2024-06-15 11:11:47 +0800

    bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -10,7 +9,6 @@ import { WorksComponent } from './works/works.component';
 @NgModule({
   declarations: [AppComponent, HeroComponent, SkillsComponent, WorksComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.ss'],
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {}

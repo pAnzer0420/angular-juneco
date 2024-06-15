7de2780e806bca3ef1f2ae5335bea6196f99dd31
angular-juneco <sheepr6@gmail.com>
2024-06-15 12:18:08 +0800

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  mode: "jit",
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{html,ts}"],
-  },
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.ts",
+    "./src/**/*.css",
+    "./src/app/**/*.ts",
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
       colors: {
         text: "#f0ebf2",
         background: "#110c14",
@@ -13,8 +18,24 @@ module.exports = {
         secondary: "#5e376d",
         accent: "#a870be",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
-  variants: {},
-  plugins: [],
+  plugins: [
+    require("postcss-purgecss")({
+      content: [
+        "./src/**/*.html",
+        "./src/**/*.ts",
+        "./src/**/*.css",
+        "./src/app/**/*.ts",
+      ],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    require("@tailwindcss/typography"),
+  ],
 };
